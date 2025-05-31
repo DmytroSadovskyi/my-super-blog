201a1b5d5ac14eede727894587fe314817567512
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2025-05-31 20:12:59 +0300

    change footer layout

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -14,12 +14,12 @@ export async function Footer() {
 
   return (
     <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
+      <div className="container py-8 gap-8 flex flex-row justify-between">
         <Link className="flex items-center font-bold text-xl" href="/">
           My blog
         </Link>
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
+        <div className="flex flex-col-reverse items-start md:flex-row md:items-center">
           <ThemeSelector />
 
           <nav className="flex flex-col md:flex-row gap-4">
